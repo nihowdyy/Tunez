1da823e2512930683326fbd2e7f5a30928783142
Gavin Li <gavin74productions@gmail.com>
2021-08-30 16:45:19 -0400

    Simple Ping-Pong command added to test the functionality

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const Discord = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 // Prefix for commands
 const prefix = '-';
@@ -13,14 +13,14 @@ client.once('ready', () => {
     console.log('Ready for some tunez!');
 });
 
-client.on('message', message => {
+client.on('messageCreate', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === 'ping') {
-        message.channel.send('pong');
+    if(command == 'ping') {
+        message.channel.send('pong!');
     }
 });
 
