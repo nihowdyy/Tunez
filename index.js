e46fc6ebc868ac4f6be6a241cc647ef6bd6e74da
Gavin Li <gavin74productions@gmail.com>
2021-08-30 16:24:23 -0400

    Removed some unnecessary git files.

---
+++ index.js
@@ -0,0 +1,28 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Prefix for commands
+const prefix = '-';
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready for some tunez!');
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping') {
+        message.channel.send('pong');
+    }
+});
+
+// Login to Discord with the token
+client.login(token);
